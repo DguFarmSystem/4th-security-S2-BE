08dfb1ca442ea9cbf4e3c62de60e16648692df6a
saokiritoni <soeun.love.lee@gmail.com>
2025-05-19 21:06:30 +0900

    [feat] #6 CORS URL 환경변수 적용

--- src/main/java/org/farmsystem/sotserver/global/config/CorsConfig.java
+++ src/main/java/org/farmsystem/sotserver/global/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package org.farmsystem.sotserver.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,6 +14,13 @@ import java.util.List;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${spring.deployment.frontend.local}")
+    private String frontendLocal;
+
+    @Value("${spring.deployment.backend.production}")
+    private String backendProduction;
+
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -21,9 +29,10 @@ public class CorsConfig implements WebMvcConfigurer {
 
         // CORS 허용할 URL
         List<String> allowedOrigins = Arrays.asList(
-                "http://localhost:5000" // 프론트 주소 추가 필요
+                "http://localhost:5173",
+                frontendLocal,
+                backendProduction
         );
-
         config.setAllowedOrigins(allowedOrigins);
 
         config.addAllowedHeader("*");
