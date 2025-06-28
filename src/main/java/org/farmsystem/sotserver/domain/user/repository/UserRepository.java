dac852d071c572536a5e4faae7aa43bb7de41d32
saokiritoni <soeun.love.lee@gmail.com>
2025-06-28 23:49:20 +0900

    [feat] #21 이메일 인증에 필요한 repository 메서드 추가

--- src/main/java/org/farmsystem/sotserver/domain/user/repository/UserRepository.java
+++ src/main/java/org/farmsystem/sotserver/domain/user/repository/UserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findBySocialId(String socialId);
+    Optional<User> findByEmail(String email);
 }
